4b80a01e34e5b0743e980d012b714dbe09eccdf8
ChoccyMilkBoi <armijoemilio08@gmail.com>
2026-04-15 23:32:34 -0500

    Im a chud^5

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
         <CourseCard 
           title="Episode 1"
           subtitle="AP Podcasting - Settle"
-          courseCode="AP English Language"
+          courseCode="AP Podcasting"
           term="2025-2026"
           color="#5f0f4e"
         />
--- src/pages/Episode1.tsx
+++ src/pages/Episode1.tsx
@@ -10,21 +10,45 @@ const Course1 = () => {
       <p>Meet your teachers in the short articles below, and go to each assignment using the links below!</p>
 
       <br></br>
+
+      <a
+        href="https://www.instagram.com/dftd_humanity/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="start-assignment-btn"
+      >
+        Visit our Instagram →
+      </a>
+
       <br></br>
       <br></br>
+
+
+      <p>What role does education play in revolution?</p>
+
       <br></br>
+
+      <p>
+        In this podcast we explore the ideas of oppression and education as portrayed by Paulo Freire, and how by guiding the masses, education can be used as a tool by which to bind 
+        the masses to their ignorance and to free them from their own limited perspectives. By educating the masses in a way that does not build a dialogue,
+        an empty sort of knowledge may be planted into the masses. Similarly to the casting of shadows in the allegory of the cave, oppressors use empty knowledge
+        to convince the oppressed of their own knowledge and prevent greater social change. Furthermore, an inability to create an effective dialogue is what acts as
+        chains upon the oppressed too.
+      </p>
       <br></br>
       <br></br>
 
       <h2>Andreia Da Silva Ekburg</h2>
+      <p>Producer, editor, cameraperson, and outline writer for the videos in this course!</p>
       <br></br>
-      <h2>Eli Newlin</h2>
-      <br></br>
-      <h2>Keira Martin</h2>
+      <h2>Keira Martin & Eli Newlin</h2>
+      <p>Podcast hosts and input on ideas in the podcast, conveying the information in this course!</p>
       <br></br>
       <h2>Emilio Armijo</h2>
+      <p>Designer of this webpage and input on ideas in podcast!</p>
       <br></br>
       <h2>Amy Torres</h2>
+      <p>Social Media, putting up posts for the podcast's instagram and producing art for the webpage and input on ideas in the podcast!</p>
       <br></br>
 
       <br></br>
